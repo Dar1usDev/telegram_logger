18b36c0802115461a5f53f2491dcd38c0d072a9e
Dmitry <66305223+Dar1usDev@users.noreply.github.com>
2024-08-11 14:40:28 +1000

    fix: add g files

---
+++ lib/src/core/models.g.dart
@@ -0,0 +1,13 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'models.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Map<String, dynamic> _$MessageDtoToJson(MessageDto instance) =>
+    <String, dynamic>{
+      'chat_id': instance.chatId,
+      'text': instance.text,
+    };
