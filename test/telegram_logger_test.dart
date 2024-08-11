598facc63abdc6fe7d8e817d2c967188cb73df13
Dmitry <66305223+Dar1usDev@users.noreply.github.com>
2024-08-11 14:27:31 +1000

    fix: remove keys :)

--- test/telegram_logger_test.dart
+++ test/telegram_logger_test.dart
@@ -1,14 +1,3 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:telegram_logger/src/telegram_logger.dart';
 
-void main() {
-  test('test log', () {
-    const botKey = '6775786476:AAGMSn9Buklx0o5YQkAnliAZYJT3S2iay6';
-    // https://t.me/c/2198841658/2
-    const chatId = '2198841658_2';
 
-    final logger = TelegramLogger(botToken: botKey, chatId: chatId);
-
-    logger.sendText('Hello');
-  });
-}
+void main() {}
